3aea1730be9bdc8b4938010992d5efd5603a98e9
Mohit Thakur <42278911+thakur-mohit@users.noreply.github.com>
2026-01-01 18:55:04 +0000

    changesetup OctoFit Tracker App backend with Django framework

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-e^9$tl6j+swtqfyoe1b0o%r*1!)0^-=1yr9^4*@bm$lm_hy$8j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.githubpreview.dev')
 
 
 # Application definition
